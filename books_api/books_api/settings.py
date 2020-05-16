c9134adc99f22965d7136817d2b11ff11511e002
Kenny <kennyaires@gmail.com>
2020-05-16 04:24:53 -0300

    Create core app and models

--- books_api/books_api/settings.py
+++ books_api/books_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core.apps.CoreConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
---
+++ books_api/core/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    edition = models.CharField(max_length=255, null=True)
+    publication_year = models.PositiveSmallIntegerField(blank=True, null=True)
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.name
