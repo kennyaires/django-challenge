dc96c067ddeb56d847e1aa0e0977e4aee098f33b
Kenny <kennyaires@gmail.com>
2020-05-17 05:24:35 -0300

    Assign name property of Author as unique

--- books_api/core/models.py
+++ books_api/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
